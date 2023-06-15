975c58ee255f7b2ed82ecfd6f927180ee1c5b501
GiselleSL <95318325+GiselleSL@users.noreply.github.com>
2023-06-15 03:05:15 -0400

    add match recipe with ingredients function

--- DataLoader.py
+++ DataLoader.py
@@ -21,11 +21,13 @@ making it easier to work with in other parts of a larger program.
 class DataLoader:
     """Clase generica para procesar la informacion ya sea de un csv o un json"""
 
-    def __init__(self, file_path, entity_class, file_type="json", entity_fields=None):
+    def __init__(
+        self, file_path, file_type="json", entity_class=None, entity_fields=None
+    ):
         self._file_path = file_path
-        self._entity_class = entity_class
         self._file_type = file_type
-        self._entity_fields = entity_fields if entity_fields is not None else []
+        self._entity_class = entity_class
+        self._entity_fields = entity_fields
 
     def map_entity_fields(self, entity_data):
         entity_args = {}
@@ -33,7 +35,7 @@ class DataLoader:
             entity_args[field] = entity_data[key]
         return entity_args
 
-    def load_entities(self):
+    def load_entities_by_class(self):
         with open(self._file_path, "r") as f:
             if self._file_type == "json":
                 data = json.load(f)
@@ -51,6 +53,27 @@ class DataLoader:
         # self.entities = entities
         return entities
 
+    def load_entities(self):
+        with open(self._file_path, "r") as f:
+            if self._file_type == "json":
+                data = json.load(f)
+                if self._entity_fields is None:
+                    return data
+                entities = [
+                    dict((k, item[k]) for k in self._entity_fields) for item in data
+                ]
+            elif self._file_type == "csv":
+                reader = csv.DictReader(f)
+                data = [dict(row) for row in reader]
+                if self._entity_fields is None:
+                    return data
+                entities = [
+                    dict((k, item[k]) for k in self._entity_fields) for item in data
+                ]
+            else:
+                raise ValueError(f"Unsupported file type: {self._file_type}")
+        return entities
+
 
 # * ------------------------------- CLASS RECIPE ------------------------------- #
 @dataclass  # decorador Python genera automáticamente los métodos __init__, __repr__, y __eq__
@@ -61,20 +84,34 @@ class Recipe:
     title: str = None
     source: str = None
     cuisine: str = None
+    ingredients: List[str] = None
 
     def __str__(self):
-        # for attr in [
-        #     "recipe_id",
-        #     "title",
-        #     "source",
-        #     "cousine",
-        # ]:
-        #     val1 = getattr(self, attr)
-        #     if val1 is not None and val1 != "":
         return f"{self.recipe_id} : {self.title} ({self.cuisine}) from {self.source}"
 
     def add_ingredients(self, ingredients_list):
         self.ingredients_list = ingredients_list
+
+    # def find_recipe_per_ingredient(self, ingredient_id):
+    #     return [
+    #         recipe
+    #         for recipe in [self]
+    #         if any(
+    #             ingredient.entity_id == ingredient_id for ingredient in self.ingredients
+    #         )
+    #     ]
+
+    def match_recipe_with_ingredients(self, relation_recipe_ingredient):
+        """
+        integra a la clase una lista con el id de los ingredientes que la componen
+        """
+        ingredients_by_recipe = []
+        for relation in relation_recipe_ingredient:
+            recipe_id_in_relation = relation["Recipe ID"]
+            if self.recipe_id == recipe_id_in_relation:
+                ingredients_by_recipe.append(relation["Entity ID"])
+
+        self.ingredients = ingredients_by_recipe
 
 
 # * ----------------------------- CLASS INGREDIENT ----------------------------- #
@@ -83,16 +120,21 @@ class Ingredient:
     """Class for representing a Ingredient"""
 
     entity_id: str = None
+    original_name: str = (
+        None  # 04 - informacion referente al nombre y la cantidad del ingrediente
+    )
     aliased_name: str = None
-    aliased_name2: str = None
-    recipe_id: int = None
-    original_name: str = None
     synonyms: List[str] = None
     category: str = None
-    compaund_name: str = None
-    synonyms_compaund: List[str] = None
+
+    def __str__(self):
+        return f"{self.entity_id} : {self.aliased_name} ({self.synonyms}) Category: {self.category}"
+
+
+# * ------------------------- CLASS COMPOUND INGREDIENT ------------------------ #
+@dataclass
+class CompoundIngredient(Ingredient):
     contituent: List[str] = None
-    compaund_category: str = None
 
 
 # * ---- Funtion for creating a list of ingredients with the same Recipe ID ---- #
@@ -114,71 +156,28 @@ def ingredientsList(ingredient, recipe_id):
             ingredients_list.append(ing)
     return ingredients_list
 
+#* --- Function for matching a list of ingredients to a corresponding recipe -- #
+def match_recipelist_with_ingredientslis(relation, recipe):
+    
+    # Crea un diccionario para almacenar los recipe_id con el mismo entity_id
+    recipe_por_entity = {}
 
-# * ----------------- Function to merge two Ingredient objects ----------------- #
-def merge_ingredients(ingredient1, ingredient2):
-    """Merge two Ingredient objects with the same entity_id into a single object.
+    # Itera sobre los datos y agrega cada recipe_id al diccionario correspondiente en recipe_por_entity
+    for info in relation:
+        recipe_id = info["Recipe ID"]
+        entity_id = info["Entity ID"]
+        if recipe_id not in recipe_por_entity:
+            recipe_por_entity[recipe_id] = [entity_id]
+        else:
+            recipe_por_entity[recipe_id].append(entity_id)
 
-    Args:
-        ingredient1 (Ingredient): The first Ingredient object to merge
-        ingredient2 (Ingredient): The second Ingredient objet to merge
-
-    Returns:
-        Ingredient: A new Ingredient object with the common attributes of the input objects.
-            If the input objects have different values for an attribute, the value from
-            ingredient2 takes precedence.
-    """
-    if ingredient1.entity_id == ingredient2.entity_id:
-        new_ingredient = Ingredient(entity_id=ingredient1.entity_id)
-
-        for attr in [
-            "recipe_id",
-            "original_name",
-            "aliased_name",
-            "aliased_name2",
-            "synonyms",
-            "category",
-            "compaund_name",
-            "synonyms_compaund",
-            "contituent",
-            "compaund_category",
-        ]:
-            val1 = getattr(ingredient1, attr)
-            val2 = getattr(ingredient2, attr)
-            if val2 is not None and val2 != "":
-                setattr(new_ingredient, attr, val2)
-            else:
-                setattr(new_ingredient, attr, val1)
-
-    else:
-        print("The ingredients do not have the same entity_id")
-    return new_ingredient
+    for rec in recipe:
+        if rec.recipe_id in recipe_por_entity:
+            rec.ingredients = recipe_por_entity[rec.recipe_id]
 
 
-def merge_ingredient_lists(ingredients1, ingredients2):
-    merged_ingredients = []
-    for ingredient1 in ingredients1:
-        found_match = False
-        for ingredient2 in ingredients2:
-            if ingredient1.entity_id == ingredient2.entity_id:
-                merged_ingredients.append(merge_ingredients(ingredient1, ingredient2))
-                found_match = True
-                break
-        if not found_match:
-            merged_ingredients.append(ingredient1)
-    for ingredient2 in ingredients2:
-        found_match = False
-        for ingredient1 in ingredients1:
-            if ingredient1.entity_id == ingredient2.entity_id:
-                found_match = True
-                break
-        if not found_match:
-            merged_ingredients.append(ingredient2)
-    return merged_ingredients
-
-
-# #* ------------------------- TESTING THE FUNTIONALITY ------------------------- #
-recipe_1 = DataLoader(
+# * ------------------------- TESTING THE FUNTIONALITY ------------------------- #
+recipe_dl = DataLoader(
     file_path="database/01_Recipe_Details.json",
     entity_class=Recipe,
     file_type="json",
@@ -190,61 +189,76 @@ recipe_1 = DataLoader(
     ],
 )
 
-ingredient_1 = DataLoader(
+ingredient_dl = DataLoader(
     file_path="database/02_Ingredients.json",
     entity_class=Ingredient,
     file_type="json",
     entity_fields=[
         ("entity_id", "Entity ID"),
+        ("category", "Category"),
+        ("synonyms", "Ingredient Synonyms"),
+        ("aliased_name", "Aliased Ingredient Name"),
     ],
 )
 
-ingredient_2 = DataLoader(
+compound_ingredient_dl = DataLoader(
     file_path="database/03_Compound_Ingredients.json",
-    entity_class=Ingredient,
+    entity_class=CompoundIngredient,
     file_type="json",
     entity_fields=[
-        ("compaund_name", "Compound Ingredient Name"),
-        ("synonyms_compaund", "Compound Ingredient Synonyms"),
+        ("aliased_name", "Compound Ingredient Name"),
+        ("synonyms", "Compound Ingredient Synonyms"),
         ("entity_id", "entity_id"),
         ("contituent", "Contituent Ingredients"),
-        ("compaund_category", "Category"),
+        ("category", "Category"),
     ],
 )
 
-ingredient_3 = DataLoader(
+relation_rec_ing_dl = DataLoader(
     file_path="database/04_Recipe-Ingredients_Aliases.json",
-    entity_class=Ingredient,
+    entity_class=None,
     file_type="json",
-    entity_fields=[
-        ("entity_id", "Entity ID"),
-        ("recipe_id", "Recipe ID"),
-        ("original_name", "Original Ingredient Name"),
-        ("aliased_name2", "Aliased Ingredient Name"),
-    ],
 )
 
+recipe = recipe_dl.load_entities_by_class()
+print("Recipe 1: ", recipe[1].recipe_id)
+ingredient = ingredient_dl.load_entities_by_class()
+print("Ingredient 1: ", ingredient[1].entity_id)
+compound_ingredient = compound_ingredient_dl.load_entities_by_class()
+print("Compound Ingredient: ", compound_ingredient[1].entity_id)
+relation = relation_rec_ing_dl.load_entities()
+print("relation: ", relation[1])
 
-# list_ingredient_1 = ingredient_1.entities[0]
-# print (list_ingredient_1.data["recipe_id"])
 
-# list_ingredient_1 = ingredient_1.entities[2]
-# print(list_ingredient_1.data)
-# print(list_ingredient_1.data["entity_id"])
+recipe[1].match_recipe_with_ingredients(relation)
+print("RECIPE 1: ", recipe[1])
+print(recipe[1].ingredients)
 
-# list_recipe_1 = recipe_1.entities[2]
-# print(list_recipe_1.data)
+# list_of_ing = ingredientsList(ingredient3, 1)
+# print (list_of_ing)
+# all_ingredients = merge_ingredient_list(ingredient2, all_ingredients)
+# print(all_ingredients[3])
 
-# print("Ingredientes1 [287]:", ingredient_1.entities[287])
-# print("Ingredientes3 [2]:", ingredient_3.entities[2])
+# # list_ingredient_1 = ingredient_1.entities[0]
+# # print (list_ingredient_1.data["recipe_id"])
 
-# merge_ingredint_291 = merge_ingredients(
-#     ingredient_1.entities[287], ingredient_3.entities[2]
-# )
-# print(merge_ingredint_291)
+# # list_ingredient_1 = ingredient_1.entities[2]
+# # print(list_ingredient_1.data)
+# # print(list_ingredient_1.data["entity_id"])
 
-# merge = merge_ingredient_lists(ingredient_1.entities, ingredient_3.entities)
-# print("Merged Ingrediets:", merge[287])
+# # list_recipe_1 = recipe_1.entities[2]
+# # print(list_recipe_1.data)
 
-ingrediente1 = recipe_1.load_entities()
-print(ingrediente1[1])
+# # print("Ingredientes1 [287]:", ingredient_1.entities[287])
+# # print("Ingredientes3 [2]:", ingredient_3.entities[2])
+
+# # merge_ingredint_291 = merge_ingredients(
+# #     ingredient_1.entities[287], ingredient_3.entities[2]
+# # )
+# # print(merge_ingredint_291)
+
+# # merge = merge_ingredient_lists(ingredient_1.entities, ingredient_3.entities)
+# # print("Merged Ingrediets:", merge[287])
+
+# ingrediente1 = recipe_1.load_entities()
+# print(ingrediente1[1])
